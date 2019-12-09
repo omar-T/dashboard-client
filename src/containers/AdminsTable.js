2f8ea408c3c40cc28fd1b06f1866c4e5d111774b
xotx <ot.themechanic95@gmail.com>
2019-12-09 18:51:15 +0300

    Add add admin functionality.

--- src/containers/AdminsTable.js
+++ src/containers/AdminsTable.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import {fetchAdmins, removeAdmin, updateAdmin} from '../store/actions/admins'
+import {createAdmin} from '../store/actions/auth'
 import {removeSuccess} from '../store/actions/successes'
 
 class AdminsTable extends Component {
-    // constructor(props){
-    //     super(props);
-    // }
-
+    constructor(props){
+        super(props);
+        this.state = {
+            newName: '',
+            newSurname: '',
+            newEmail: '',
+            newPassword: ''
+        }
+    }
     componentDidMount(){
         this.props.fetchAdmins();
     }
@@ -26,12 +32,33 @@ class AdminsTable extends Component {
         });
     }
 
-    handleSubmit = (admin_id) => {
-        this.props.updateAdmin({...this.state, _id: admin_id});
+    handleUpdate = (admin_id) => {
+        this.props.updateAdmin({
+            name: this.state.name,
+            surname: this.state.surname,
+            email: this.state.email,
+            password: this.state.password,
+            _id: admin_id
+        });
+    }
+
+    handleAdd = () => {
+        this.props.createAdmin({
+            name: this.state.newName,
+            surname: this.state.newSurname,
+            email: this.state.newEmail,
+            password: this.state.newPassword
+        })
+            .then(() => {
+                this.props.fetchAdmins();
+            })
+            .catch(() => {
+                return;
+            });
     }
 
     render() {
-        // const {password} = this.state;
+        const {newName, newSurname, newEmail} = this.state;
         const {admins, successes, removeAdmin, updateAdmin} = this.props;
         let adminTDS = admins.map(admin => (
             <tr key={admin._id}>
@@ -39,8 +66,8 @@ class AdminsTable extends Component {
                 <td>{admin.surname}</td>
                 <td>{admin.email}</td>
                 <td>
-                    <button className='btn btn-outline-danger mr-3' data-toggle='modal' data-target='#deleteAdmin'>Delete</button>
-                    <div className="modal fade" id="deleteAdmin" tabIndex="-1" role="dialog" aria-labelledby="deleteAdminLabel" aria-hidden="true">
+                    <button className='btn btn-outline-danger mr-3' data-toggle='modal' data-target={`#deleteAdmin_${admin._id}`}>Delete</button>
+                    <div className="modal fade" id={`deleteAdmin_${admin._id}`} tabIndex="-1" role="dialog" aria-labelledby="deleteAdminLabel" aria-hidden="true">
                         <div className="modal-dialog" role="document">
                             <div className="modal-content">
                                 <div className="modal-header">
@@ -124,13 +151,12 @@ class AdminsTable extends Component {
                                                     name='password'
                                                     required
                                                     onChange={this.handleChange}
-                                                    // value={password}
                                                 />
                                             </div>
                                     </div>
                                     <div className="modal-footer">
                                         <button className="btn btn-secondary" data-dismiss="modal">Close</button>
-                                        <button onClick={this.handleSubmit.bind(this, admin._id)} className="btn btn-success" data-dismiss='modal'>Update Admin</button>
+                                        <button onClick={this.handleUpdate.bind(this, admin._id)} className="btn btn-success" data-dismiss='modal'>Update Admin</button>
                                     </div>
                                 </form>
                             </div>
@@ -150,6 +176,84 @@ class AdminsTable extends Component {
                 {successes.message && 
                     <div className='alert alert-success mx-3'>{successes.message}</div>
                 }
+                <button className='btn btn-success float-right' data-toggle='modal' data-target='#addAdmin'><i className="fas fa-plus mr-2"></i> Add New Admin</button>
+                <div className="modal fade" id='addAdmin' tabIndex="-1" role="dialog" aria-labelledby="addAdminLabel" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="addAdminLabel">Add Admin Form</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <form>
+                                <div className="modal-body">
+                                        <div className='input-group-prepend mb-3'>
+                                            <span className='input-group-text'>
+                                                <i className="far fa-user"></i>
+                                            </span>
+                                            <input 
+                                                className='form-control' 
+                                                type='text' 
+                                                placeholder='Name'
+                                                name='newName'
+                                                required
+                                                onChange={this.handleChange}
+                                                value={newName}
+                                            />
+                                        </div>
+                                        <div className='input-group-prepend mb-3'>
+                                            <span className='input-group-text'>
+                                                <i className="far fa-user"></i>
+                                            </span>
+                                            <input 
+                                                className='form-control' 
+                                                type='text' 
+                                                placeholder='Surname'
+                                                name='newSurname'
+                                                required
+                                                onChange={this.handleChange}
+                                                defaultValue={newSurname}
+                                            />
+                                        </div>
+                                        <div className='input-group-prepend mb-3'>
+                                            <span className='input-group-text email'>
+                                                <i className="far fa-envelope-open"></i>
+                                            </span>
+                                            <input 
+                                                className='form-control' 
+                                                type='email' 
+                                                placeholder='Email'
+                                                name='newEmail'
+                                                required
+                                                onChange={this.handleChange}
+                                                defaultValue={newEmail}
+                                            />
+                                        </div>
+                                        <div className='input-group-prepend mb-4'>
+                                            <span className='input-group-text'>
+                                                <i className="fas fa-lock"></i>
+                                            </span>
+                                            <input 
+                                                className='form-control' 
+                                                type='password' 
+                                                placeholder='Password'
+                                                name='newPassword'
+                                                required
+                                                onChange={this.handleChange}
+                                            />
+                                        </div>
+                                </div>
+                                <div className="modal-footer">
+                                    <button className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button onClick={this.handleAdd} className="btn btn-success" data-dismiss='modal'>Add Admin</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <h2>Admin Table</h2>
+                <hr/>
                 <table className='table table-responsive-sm table-hover mb-0'>
                     <thead className='thead-dark'>
                         <tr>
@@ -175,4 +279,4 @@ function mapStateToProps(state){
     }
 }
 
-export default connect(mapStateToProps, {fetchAdmins, removeAdmin, updateAdmin, removeSuccess})(AdminsTable);
+export default connect(mapStateToProps, {fetchAdmins, removeAdmin, updateAdmin, removeSuccess, createAdmin})(AdminsTable);
