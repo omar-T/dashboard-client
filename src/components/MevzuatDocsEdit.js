8841f73930f3e5f31e4ba5e54f0993cb47e2a546
xotx <ot.themechanic95@gmail.com>
2019-12-26 16:39:09 +0300

    Add react scroll for page navigation. Add some styles.

--- src/components/MevzuatDocsEdit.js
+++ src/components/MevzuatDocsEdit.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react'
+import {Link, Element} from 'react-scroll'
 import ContentEditable from 'react-contenteditable'
 import './MevzuatDocsEdit.css'
 // import SanitizeHtml from 'sanitize-html'
@@ -152,22 +153,23 @@ class MevzuatDocsEdit extends Component {
     prepareTitleText = (t) => {
         const {editable} = this.state;
         let mainTitle = t.text.map((pt, index) => {
-                const title = `<h4 class='text-center' id='${index}'>${pt.text}</h4>`;
+                // const title = `<strong>${pt.text}</strong>`;
                 return (
                     <Fragment>
                         {!editable &&
                             <div className='float-right pt-1 pr-1'>
                                 <button className='btn btn-outline-success btn-sm mr-1' onClick={this.handleUpdate.bind(this, t.id, t.type, index)}>Update</button>
-                                <button className='btn btn-outline-danger btn-sm' onClick={this.handleDelete.bind(this, t.id, 'text', index)}>Delete</button>
+                                <button className='btn btn-outline-danger btn-sm mr-1' onClick={this.handleDelete.bind(this, t.id, 'text', index)}>Delete</button>
+                                <button className='btn btn-outline-primary btn-sm'>Add New Madde</button>
                             </div>
                         }
                         
                         <ContentEditable
                             key={index}
-                            className='editable'
-                            tagName='div'
+                            className='editable text-center font-weight-bold'
+                            tagName='h4'
                             id={index}
-                            html={title}
+                            html={pt.text}
                             disabled={editable}
                             name={`${t.id}_${index}_${t.type}`}
                         />
@@ -176,10 +178,10 @@ class MevzuatDocsEdit extends Component {
         });
         // console.log('Parent Title: ', mainTitle);
         return (
-            <div key={t.id} id={t.id} onChange={this.handleChange}>
+            <Element key={t.id} id={t.id} name={t.id} onChange={this.handleChange}>
                 {mainTitle}
                 <br/>
-            </div>
+            </Element>
         )
     }
 
@@ -192,7 +194,8 @@ class MevzuatDocsEdit extends Component {
                     {!editable &&
                         <div className='float-right pt-1 pr-1'>
                             <button className='btn btn-outline-success btn-sm mr-1' onClick={this.handleUpdate.bind(this, t.id, t.type, index)}>Update</button>
-                            <button className='btn btn-outline-danger btn-sm' onClick={this.handleDelete.bind(this, t.id, 'text', index)}>Delete</button>
+                            <button className='btn btn-outline-danger btn-sm mr-1' onClick={this.handleDelete.bind(this, t.id, 'text', index)}>Delete</button>
+                            <button className='btn btn-outline-primary btn-sm'>Add New Madde</button>
                         </div>
                     }
                     <ContentEditable
@@ -203,27 +206,31 @@ class MevzuatDocsEdit extends Component {
                         html={txt}
                         disabled={editable}
                         name={`${t.id}_${index}_${t.type}`}
-                        onChange={this.handleChange}
                     />
                 </Fragment>
             )
         });
+        // let maddeTitle = `<strong>${t.title}</strong>`;
+        // let maddeBaslik = '';
+        // if(t.madde_baslik){
+        //     maddeBaslik = `<strong>${t.madde_baslik.text}</strong>`;
+        // }
         return (
             <div key={t.id} id={t.id}>
                 {!editable &&
                     <div className='float-right pt-1 pr-1'>
                         <button className='btn btn-outline-success btn-sm mr-1' onClick={this.handleUpdate.bind(this, t.id, 'title')}>Update</button>
-                        <button className='btn btn-outline-danger btn-sm' onClick={this.handleDelete.bind(this, t.id, 'title')}>Delete</button>
+                        <button className='btn btn-outline-danger btn-sm mr-1' onClick={this.handleDelete.bind(this, t.id, 'title')}>Delete</button>
+                        <button className='btn btn-outline-primary btn-sm'>Add New Madde</button>
                     </div>
                 }
                 <ContentEditable
                     id={`${prefix}Header`}
-                    className='editable'
+                    className='editable font-weight-bold'
                     tagName='h4'
                     html={t.title}
                     disabled={editable}
                     name={`${t.id}_title`}
-                    onChange={this.handleChange}
                 />
                 <hr/>
                 {t.madde_baslik &&
@@ -231,17 +238,17 @@ class MevzuatDocsEdit extends Component {
                         {!editable && 
                             <div className='float-right pt-1 pr-1'>
                                 <button className='btn btn-outline-success btn-sm mr-1' onClick={this.handleUpdate.bind(this, t.id, 'madde_baslik')}>Update</button>
-                                <button className='btn btn-outline-danger btn-sm' onClick={this.handleDelete.bind(this, t.id, 'madde_baslik')}>Delete</button>
+                                <button className='btn btn-outline-danger btn-sm mr-1' onClick={this.handleDelete.bind(this, t.id, 'madde_baslik')}>Delete</button>
+                                <button className='btn btn-outline-primary btn-sm'>Add New Madde</button>
                             </div>
                         }
                         <ContentEditable
                             id={`${prefix}Title`}
-                            className='text-center editable'
+                            className='editable text-center font-weight-bold'
                             tagName='h5'
                             html={t.madde_baslik.text}
                             disabled={editable}
                             name={`${t.id}_madde_baslik`}
-                            onChange={this.handleChange}
                         />
                     </Fragment>
                 }
@@ -253,6 +260,7 @@ class MevzuatDocsEdit extends Component {
 
     render() {
         const {head, text, editable} = this.state;
+        const {mevDoc} = this.props.foundDocs;
         console.log(text);
         let mevText = text.map(t => {
             switch(t.type){
@@ -278,15 +286,46 @@ class MevzuatDocsEdit extends Component {
         console.log(mevText);
         return (
             <div className='container-fluid'>
-                <div className='container bg-white py-3'>
+                <div className='container bg-white py-2'>
+                    <nav className='navbar doc_navbar pl-0'>
+                        <ul className='navbar-nav mr-auto'>
+                            <li className='nav-item'>
+                                <button className='nav-link index-button' data-toggle="collapse" data-target="#collapseIndex" aria-expanded="true" aria-controls="collapseIndex">
+                                    Indexes
+                                </button>
+                            </li>
+                        </ul>
+                    </nav>
+                    {mevDoc &&
+                        <div id="collapseIndex" className="collapse">
+                            <ul className='index text-center'>
+                                {mevDoc.index.map(ind => 
+                                    <li className='index-link'>
+                                        <Link 
+                                            to={`${ind.madde_id}`}
+                                            spy={true} 
+                                            smooth={true} 
+                                            duration={500}
+                                        >
+                                            {ind.text}
+                                        </Link>
+                                    </li>
+                                )}
+                            </ul>
+                        </div>
+                    }
+                    {/* <h5 className='font-weight-bold' data-toggle="collapse" data-target="#collapseIndex" aria-expanded="true" aria-controls="collapseIndex">Indexes:</h5> */}
+                    
+                </div>
+                <div className='container bg-white my-2 py-3'>
                     {head !== '' &&
-                        <h4>
+                        <h5>
                             <strong>{head}</strong>
                             {editable ?
                                 <button className='btn btn-outline-dark float-right' onClick={this.toggleEditable}>Enable Editing</button> :
                                 <button className='btn btn-outline-danger float-right' onClick={this.toggleEditable}>Disable Editing</button>
                             }
-                        </h4>
+                        </h5>
                     }
                     <hr/>
                     {mevText}
