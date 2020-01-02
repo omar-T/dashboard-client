6c41fa8ba3e3372f2496badc8c7d166dccd0502c
xotx <ot.themechanic95@gmail.com>
2020-01-02 14:36:40 +0300

    Refactor redux dev tool.

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ import thunk from 'redux-thunk'
 export function configureStore(){
     const store = createStore(rootReducer, compose(
         applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
     ));
     return store;
 }
