5e189c9d670dda2ef938f2c41d25692cf6bbcc0c
xotx <ot.themechanic95@gmail.com>
2020-01-26 21:17:32 +0300

    Add component did mount to use handleGetActivityLastFiveDays.

--- src/components/FiveDaysDocChart.js
+++ src/components/FiveDaysDocChart.js
@@ -1,79 +1,88 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
 import Moment from 'moment'
+import {handleGetActivityLastFiveDays} from '../store/actions/dashboardData'
 import {Line} from 'react-chartjs-2'
 
 class LogChart extends Component {
-
-    getDates = (diff, type) => {
-        let dateLabels = [];
-        const end = Moment();
-        let start = Moment().subtract(diff, type);
-        for(let m = start; m.diff(end, type) <= 0; m.add(1, type)){
-            dateLabels.push(new Date(m));
-        }
-        return dateLabels;
+    componentDidMount(){
+        const {handleGetActivityLastFiveDays} = this.props;
+        handleGetActivityLastFiveDays();
     }
-
+    
     setData = () => {
-        let labels = this.getDates(4, 'days');
-        let data = [];
-        labels.forEach(d => {
-            const logData = this.props.logs.filter(l => Moment(l.createdAt).format('YYYY-MM-DD') === Moment(d).format('YYYY-MM-DD'));
-            data.push(logData.length);
-        });
-        return {
-            labels,
-            datasets: [
-                {
-                    label: 'Document Count',
-                    borderColor: 'rgba(190, 229, 235, 1)',
-                    backgroundColor: 'rgba(190, 229, 235, 0.2)',
-                    data
-                }
-            ]
-        }
+        const {dashboardData} = this.props;
+        console.log(dashboardData);
+        if(dashboardData.fiveDaysLogs !== undefined){
+            let labels = [];
+            let data = [];
+            dashboardData.fiveDaysLogs.forEach(log => {
+                labels.push(log.date);
+                data.push(log.count);
+            });
+            return {
+                labels,
+                datasets: [
+                    {
+                        label: 'Document Count',
+                        borderColor: 'rgba(190, 229, 235, 1)',
+                        backgroundColor: 'rgba(190, 229, 235, 0.2)',
+                        data
+                    }
+                ]
+            }
+        } 
     }
 
     render() {
+        const {dashboardData} = this.props;
         return (
             <div className='col-12 col-lg-6 col-xl-4 mb-2 mb-lg-0'>
                 <div className='bg-light p-4'>
                     <h4>Users Activity For Last 5 Days</h4>
                     <hr/>
-                    <Line 
-                        data={this.setData}
-                        options={{
-                            responsive: true,
-                            scales: {
-                                xAxes: [{
-                                    gridLines: {
-                                        drawOnChartArea: false
-                                    },
-                                    display: true,
-                                    type: 'time',
-                                    distribution: 'series',
-                                    time: {
-                                        tooltipFormat: 'DD-MM-YYYY',
-                                        unit: 'day',
-                                        stepSize: 1,
-                                        displayFormats: {
-                                            day: 'DD MMM'
+                    {dashboardData.fiveDaysLogs !== undefined && 
+                        <Line 
+                            data={this.setData}
+                            options={{
+                                responsive: true,
+                                scales: {
+                                    xAxes: [{
+                                        gridLines: {
+                                            drawOnChartArea: false
+                                        },
+                                        display: true,
+                                        type: 'time',
+                                        distribution: 'series',
+                                        time: {
+                                            tooltipFormat: 'DD-MM-YYYY',
+                                            unit: 'day',
+                                            stepSize: 1,
+                                            displayFormats: {
+                                                day: 'DD MMM'
+                                            }
                                         }
-                                    }
-                                }],
-                                yAxes: [{
-                                    ticks: {
-                                        beginAtZero: true,
-                                        maxTicksLimit: 6
-                                    }
-                                }]
-                            }
-                        }}
-                    />
+                                    }],
+                                    yAxes: [{
+                                        ticks: {
+                                            beginAtZero: true,
+                                            maxTicksLimit: 6
+                                        }
+                                    }]
+                                }
+                            }}
+                        />
+                    }
                 </div>
             </div>
         )
     }
 }
 
-export default LogChart;
+function mapStateToProps(state){
+    return {
+        dashboardData: state.dashboardData
+    }
+}
+
+export default connect(mapStateToProps, {handleGetActivityLastFiveDays})(LogChart);
