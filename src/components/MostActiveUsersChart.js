45cb9983785fce5a9c67bbfc95d66cbcfcf1f0c3
xotx <ot.themechanic95@gmail.com>
2020-01-26 20:15:12 +0300

    Refactor component to use handleGetMostActiveUsers.

--- src/components/MostActiveUsersChart.js
+++ src/components/MostActiveUsersChart.js
@@ -1,69 +1,74 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import {handleGetMostActiveUsers} from '../store/actions/dashboardData'
 import {Bar} from 'react-chartjs-2'
 
-export default class MostActiveUsersChart extends Component {
+class MostActiveUsersChart extends Component {
+    componentDidMount(){
+        const {handleGetMostActiveUsers} = this.props;
+        handleGetMostActiveUsers();
+    }
+    
     setData = () => {
-        let countData = this.props.users.map(u => {
-            let docsCount = this.props.logs.filter(l => l.userId === u._id);
-            return{
-                ...u,
-                count: docsCount.length
-            };
-        });
-        countData.sort((a, b) => {
-            const countA = a.count;
-            const countB = b.count;
-            if (countA > countB) {
-                return -1;
+        const {dashboardData} = this.props;
+        if(dashboardData.usersData !== undefined){
+            let labels = [];
+            let data =[];
+            dashboardData.usersData.forEach(user => {
+                labels.push(`${user.name} ${user.surname}`);
+                data.push(user.count);
+            });
+            return {
+                labels,
+                datasets: [
+                    {
+                        label: 'Clicked Docs Count',
+                        backgroundColor: 'rgba(0, 153, 255, 0.85)',
+                        data
+                    }
+                ]
             }
-            if (countB > countA) {
-                return 1;
-            }
-            return 0;
-        });
-        let activeUsers = countData.slice(0, 4);
-        let labels = activeUsers.map(au => `${au.name} ${au.surname}`);
-        let data = activeUsers.map(au => au.count);
-        return {
-            labels,
-            datasets: [
-                {
-                    label: 'Clicked Docs Count',
-                    backgroundColor: 'rgba(0, 153, 255, 0.85)',
-                    data
-                }
-            ]
         }
     }
     
     render() {
-        this.setData();
+        const {dashboardData} = this.props;
         return (
             <div className='col-12 col-lg-6 col-xl-4 mb-2 mb-lg-0'>
                 <div className='bg-light p-4'>
                     <h4>Most Active Users</h4>
                     <hr/>
-                    <Bar
-                        data={this.setData}
-                        options={{
-                            responsive: true,
-                            scales: {
-                                xAxes: [{
-                                    gridLines: {
-                                        drawOnChartArea: false
-                                    }
-                                }],
-                                yAxes: [{
-                                    ticks: {
-                                        beginAtZero: true,
-                                        maxTicksLimit: 6
-                                    }
-                                }]
-                            }
-                        }}
-                    />
+                    {dashboardData.usersData !== undefined && 
+                        <Bar
+                            data={this.setData}
+                            options={{
+                                responsive: true,
+                                scales: {
+                                    xAxes: [{
+                                        gridLines: {
+                                            drawOnChartArea: false
+                                        }
+                                    }],
+                                    yAxes: [{
+                                        ticks: {
+                                            beginAtZero: true,
+                                            maxTicksLimit: 6
+                                        }
+                                    }]
+                                }
+                            }}
+                        />
+                    }
                 </div>
             </div>
         )
     }
 }
+
+function mapStateToProps(state){
+    return {
+        dashboardData: state.dashboardData
+    }
+}
+
+export default connect(mapStateToProps, {handleGetMostActiveUsers})(MostActiveUsersChart);
