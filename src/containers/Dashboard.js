8af03ac25985b9ee5c14336f7f35b4ad7ddbc704
xotx <ot.themechanic95@gmail.com>
2020-01-26 20:13:35 +0300

    Refactor and add Dashboard.js component.

---
+++ src/containers/Dashboard.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import {fetchLogs} from '../store/actions/logs'
+import {handleGetUsersCount} from '../store/actions/dashboardData'
+import FiveDaysDocChart from '../components/FiveDaysDocChart'
+import FourWeeksDocChart from '../components/FourWeeksDocChart'
+import MostActiveUsersChart from '../components/MostActiveUsersChart'
+
+class Dashboard extends Component {
+    componentDidMount(){
+        const {handleGetUsersCount, fetchLogs} = this.props;
+        fetchLogs();
+        handleGetUsersCount();
+    }
+
+    render() {
+        const {dashboardData, logs} = this.props;
+        return (
+            <div className='container-fluid'>
+                <div className='row'>
+                    <div className='col-4'>
+                        <div className='card bg-light'>
+                            <div className='card-body'>
+                                <span className='float-right'><i className="fas fa-users fa-4x"></i></span>
+                                <h4 className='card-title'>Total Users</h4>
+                                <h3 className='card-text'>{dashboardData.usersCount}</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='row my-3'>
+                    <FiveDaysDocChart 
+                        logs={logs} 
+                        {...this.props}
+                    />
+                    <FourWeeksDocChart 
+                        logs={logs} 
+                        {...this.props}
+                    />
+                    <MostActiveUsersChart 
+                        {...this.props}
+                    />
+                </div>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state){
+    return {
+        dashboardData: state.dashboardData,
+        logs: state.logs
+    }
+}
+
+export default connect(mapStateToProps, {handleGetUsersCount, fetchLogs})(Dashboard);
--- src/containers/Homepage.js
+++ src/containers/Homepage.js
@@ -1,59 +1,21 @@
 import React, {Component} from 'react'
 import {Switch, Route, withRouter} from 'react-router-dom'
-import {connect} from 'react-redux'
 import './Homepage.css'
-import {fetchLogs} from '../store/actions/logs'
-import {fetchUsers} from '../store/actions/users'
 import AdminsTable from './AdminsTable'
 import UsersTable from './UsersTable'
-import FiveDaysDocChart from '../components/FiveDaysDocChart'
-import FourWeeksDocChart from '../components/FourWeeksDocChart'
-import MostActiveUsersChart from '../components/MostActiveUsersChart'
+import Dashboard from '../containers/Dashboard'
 import Doc from './Doc'
 import IctihatDocsEdit from './IctihatDocsEdit'
 import MevzuatDocsEdit from './MevzuatDocsEdit'
 
 class Homepage extends Component {
-    componentDidMount(){
-        this.props.fetchLogs();
-        this.props.fetchUsers();
-    }
-
     render(){
-        const {logs, users} = this.props;
         return (
             <div className='px-4 main'>
                 <Switch>
                     <Route
                         exact path='/' render={(props) => (
-                            <div className='container-fluid'>
-                                <div className='row'>
-                                    <div className='col-4'>
-                                        <div className='card bg-light'>
-                                            <div className='card-body'>
-                                                <span className='float-right'><i className="fas fa-users fa-4x"></i></span>
-                                                <h4 className='card-title'>Total Users</h4>
-                                                <h3 className='card-text'>{users.length}</h3>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className='row my-3'>
-                                    <FiveDaysDocChart 
-                                        logs={logs} 
-                                        {...props}
-                                    />
-                                    <FourWeeksDocChart 
-                                        logs={logs} 
-                                        {...props}
-                                    />
-                                    <MostActiveUsersChart 
-                                        logs={logs} 
-                                        users={users} 
-                                        {...props}
-                                    />
-                                </div>
-                            </div>
+                            <Dashboard {...props}/>
                         )}
                     />
                     <Route
@@ -88,11 +50,6 @@ class Homepage extends Component {
     }
 }
 
-function mapStateToProps(state){
-    return {
-        logs: state.logs,
-        users: state.users
-    }
-}
 
-export default withRouter(connect(mapStateToProps, {fetchLogs, fetchUsers})(Homepage));
+
+export default withRouter(Homepage);
