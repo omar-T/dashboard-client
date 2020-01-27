468706b39dff9b5d85b2f0a71f8d8cd98564bf37
xotx <ot.themechanic95@gmail.com>
2020-01-27 14:43:31 +0300

    Refactor to use user property.

--- src/components/UserCharts.js
+++ src/components/UserCharts.js
@@ -2,17 +2,21 @@ import React, { Fragment } from 'react'
 import FiveDaysUserActivity from '../containers/FiveDaysUserActivity'
 import FourWeekssUserActivity from '../containers/FourWeeksUserActivity'
 
-const UserCharts = ({chartOwner, dataFiveDays, dataFourWeeks}) => {
+const UserCharts = ({user, dataFiveDays, dataFourWeeks}) => {
     return (
         <Fragment>
             <div className='bg-light p-3 mb-3'>
-                <h4>Details for {chartOwner}</h4>
+                <h4>
+                    Details for {Object.keys(user).length !== 0 && `${user.name} ${user.surname}`}
+                </h4>
             </div>
             <div className='row'>
                 <FiveDaysUserActivity
+                    user_id={user._id}
                     dataFiveDays={dataFiveDays}
                 />
                 <FourWeekssUserActivity
+                    user_id={user._id}
                     dataFourWeeks={dataFourWeeks}
                 />
             </div>
