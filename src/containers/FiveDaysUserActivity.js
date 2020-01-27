163f3f3dcd0f026b0b116d91cba7f9285d202e61
xotx <ot.themechanic95@gmail.com>
2020-01-27 16:45:20 +0300

    Add componentDidUpdate to get the user log data. Add setData to set data for the chart.

--- src/containers/FiveDaysUserActivity.js
+++ src/containers/FiveDaysUserActivity.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
 import {Line} from 'react-chartjs-2'
-
+import {handleGetUserActivityLastFiveDays} from '../store/actions/userLogs'
 class FiveDaysUserActivity extends Component {
-    
+    componentDidUpdate(prevProps){
+        const {user_id, handleGetUserActivityLastFiveDays} = this.props;
+        if(user_id && prevProps.user_id !== user_id){
+            handleGetUserActivityLastFiveDays(user_id);
+        }
+    }
+
+    setData = () => {
+        const {userLogs} = this.props;
+        let labels = [];
+        let data = [];
+        userLogs.fiveDaysLogs.forEach(log => {
+            labels.push(log.date);
+            data.push(log.count);
+        });
+        return {
+            labels,
+            datasets: [
+                {
+                    label: 'Document Count',
+                    borderColor: 'rgba(190, 229, 235, 1)',
+                    backgroundColor: 'rgba(190, 229, 235, 0.2)',
+                    data
+                }
+            ]
+        }
+    }
 
     render() {
-        const {dataFiveDays} = this.props;
+        const {userLogs} = this.props;
         return (
             <div className='col-12 col-lg-6 mb-2 mb-lg-0'>
                 <div className='p-4 bg-light'>
                     <h5>Activity For Last 5 Days</h5>
                     <hr/>
                     <Line
-                        data={dataFiveDays}
+                        data={userLogs.fiveDaysLogs ? this.setData : {}}
                         options={{
                             responsive: true,
                             scales: {
@@ -47,4 +74,10 @@ class FiveDaysUserActivity extends Component {
     }
 }
 
-export default FiveDaysUserActivity;
+function mapStateToProps(state){
+    return {
+        userLogs: state.userLogs
+    }
+}
+
+export default connect(mapStateToProps, {handleGetUserActivityLastFiveDays})(FiveDaysUserActivity);
