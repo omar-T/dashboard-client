0d57f28a6c62f1442e86272bbdbb9d8102918348
xotx <ot.themechanic95@gmail.com>
2020-01-27 11:31:56 +0300

    Refactor to use handleGetActivityLastFourWeeks functionality.

--- src/components/FourWeeksDocChart.js
+++ src/components/FourWeeksDocChart.js
@@ -1,80 +1,86 @@
 import React, { Component } from 'react'
-import Moment from 'moment'
+import {connect} from 'react-redux'
+import {handleGetActivityLastFourWeeks} from '../store/actions/dashboardData'
 import {Line} from 'react-chartjs-2'
 
-export default class FourWeeksDocChart extends Component {
-    getDates = (diff, type) => {
-        let dateLabels = [];
-        const end = Moment();
-        let start = Moment().subtract(diff, type);
-        for(let m = start; m.diff(end, type) <= 0; m.add(1, type)){
-            dateLabels.push(new Date(m));
-        }
-        return dateLabels;
+class FourWeeksDocChart extends Component {
+    componentDidMount(){
+        const {handleGetActivityLastFourWeeks} = this.props;
+        handleGetActivityLastFourWeeks();
     }
 
     setData = () => {
-        let labels = this.getDates(4, 'weeks');
-        let data = labels.map((d, i, dates) => {
-            let count = 0;
-            this.props.logs.forEach(l => {
-                if(Moment(l.createdAt).isBetween(Moment(d), Moment(dates[i+1]))){
-                    count++;
-                }
+        const {dashboardData} = this.props;
+        if(dashboardData.fourWeeksLogs !== undefined){
+            let labels = [];
+            let data = [];
+            dashboardData.fourWeeksLogs.forEach(log => {
+                labels.push(log.date);
+                data.push(log.count);
             });
-            return count;
-        });
-        return {
-            labels,
-            datasets: [
-                {
-                    label: 'Document Count',
-                    borderColor: 'rgba(190, 229, 235, 1)',
-                    backgroundColor: 'rgba(190, 229, 235, 0.2)',
-                    data
-                }
-            ]
+            return {
+                labels,
+                datasets: [
+                    {
+                        label: 'Document Count',
+                        borderColor: 'rgba(190, 229, 235, 1)',
+                        backgroundColor: 'rgba(190, 229, 235, 0.2)',
+                        data
+                    }
+                ]
+            }
         }
     }
 
     render() {
+        const {dashboardData} = this.props;
         return (
             <div className='col-12 col-lg-6 col-xl-4 mb-2 mb-lg-0'>
                 <div className='bg-light p-4'>
-                    <h4>Users Activity For Last 4 Weeks</h4>
+                    <h5>Users Activity For Last 4 Weeks</h5>
                     <hr/>
-                    <Line 
-                        data={this.setData}
-                        options={{
-                            responsive: true,
-                            scales: {
-                                xAxes: [{
-                                    gridLines: {
-                                        drawOnChartArea: false
-                                    },
-                                    display: true,
-                                    type: 'time',
-                                    distribution: 'series',
-                                    time: {
-                                        tooltipFormat: 'DD-MM-YYYY',
-                                        unit: 'day',
-                                        stepSize: 1,
-                                        displayFormats: {
-                                            day: 'DD MMM'
+                    {dashboardData.fourWeeksLogs !== undefined && 
+                        <Line 
+                            data={this.setData}
+                            options={{
+                                responsive: true,
+                                scales: {
+                                    xAxes: [{
+                                        gridLines: {
+                                            drawOnChartArea: false
+                                        },
+                                        display: true,
+                                        type: 'time',
+                                        distribution: 'series',
+                                        time: {
+                                            tooltipFormat: 'DD-MM-YYYY',
+                                            unit: 'day',
+                                            stepSize: 1,
+                                            displayFormats: {
+                                                day: 'DD MMM'
+                                            }
                                         }
-                                    }
-                                }],
-                                yAxes: [{
-                                    ticks: {
-                                        beginAtZero: true,
-                                        maxTicksLimit: 6
-                                    }
-                                }]
-                            }
-                        }}
-                    />
+                                    }],
+                                    yAxes: [{
+                                        ticks: {
+                                            beginAtZero: true,
+                                            maxTicksLimit: 6
+                                        }
+                                    }]
+                                }
+                            }}
+                        />
+                    }
                 </div>
             </div>
         )
     }
 }
+
+function mapStateToProps(state){
+    return {
+        dashboardData: state.dashboardData
+    }
+}
+
+export default connect(mapStateToProps, {handleGetActivityLastFourWeeks})(FourWeeksDocChart);
